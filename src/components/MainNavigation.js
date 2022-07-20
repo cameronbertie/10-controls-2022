b865818ce8fdf7a6dd77b3ce427014df680c59aa
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-07-20 18:51:33 +0100

    main navigation layout

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/MainNavigation.css';
+
+export default function MainNavigation() {
+  return (
+    <nav>
+        <div class="border-box">LEFT</div>
+        <div class="main-navigation-content">NAV</div>
+        <div class="border-box">RIGHT</div>
+    </nav>
+  )
+}
