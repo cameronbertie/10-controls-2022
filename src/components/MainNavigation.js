e6b29ce9271998b9fc2378d378b14ef1fb8579d8
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-07-20 23:54:14 +0100

    side navigation layout

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import '../stylesheets/MainNavigation.css';
+import React from "react";
+import logo from "../svg/logo.svg";
+import bars from "../svg/bars.svg";
+import discordLogo from "../svg/discord-logo.svg";
+import "../stylesheets/MainNavigation.css";
 
 export default function MainNavigation() {
   return (
-    <nav>
-        <div class="border-box">LEFT</div>
-        <div class="main-navigation-content">NAV</div>
-        <div class="border-box">RIGHT</div>
-    </nav>
-  )
+    <>
+    <nav class="main-site-nav">
+      <div class="border-box-left">
+        <img class="navigation-logo" src={logo} alt="10 Controls Logo" />
+      </div>
+      <div class="main-navigation-content">
+        <div class="main-navigation-link hvr-underline-from-center">Rocket League</div>
+        <div class="main-navigation-link">F1 22</div>
+      </div>
+      <div class="border-box-right"><img class="menu-icon" src={bars} alt="Menu Icon" /></div>
+      </nav>
+      <nav class="left-site-nav">
+      <img class="discord-logo" src={discordLogo} alt="Discord Logo" />
+      </nav>
+      <nav class="right-site-nav">
+      nav
+      </nav>
+      </>
+  );
 }
