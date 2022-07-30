ac8721818af31cb8b212809c919fb92476ccf9bb
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-07-30 19:53:35 +0100

    started work on homepage

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import './App.css';
-import Navigation from './components/Navigation';
-import F1Main from './components/F1Main';
-import F1Races from './components/F1Races';
-import F1Register from './components/F1Register';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import F1Main from "./components/F1Main";
+import F1Races from "./components/F1Races";
+import F1Register from "./components/F1Register";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App () {
+function App() {
   return (
     <Router>
-    <div>
-      <header>
-        <Navigation />
+      <div>
+        <header>
+          <Navigation />
         </header>
         <main>
-        <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/f1" element={<F1Main/>} />
-        <Route path="/f1/races" element={<F1Races/>} />
-        <Route path="/f1/register" element={<F1Register/>} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/f1" element={<F1Main />} />
+            <Route path="/f1/races" element={<F1Races />} />
+            <Route path="/f1/register" element={<F1Register />} />
+          </Routes>
         </main>
-  </div>
+      </div>
     </Router>
-  )
+  );
 }
 
-const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-)
-
-export default App
+export default App;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,36 +4,37 @@ import '../stylesheets/Menu.css'
 export default function Menu () {
   return (
     <div className="menu-container">
+      <div className="menu-outer-container">
         <div className="menu-inner-container">
             <nav className="category-content">
-                <div className="category-heading category-item">Rocket League</div>
+                <div className="category-heading"><a className="category-item">Rocket League</a></div>
                 <hr className="menu-hr"></hr>
                 <div className="category-list">
-                <div className="category-subheading category-item">Tournaments</div>
+                <div className="category-subheading"><a className="category-item">Tournaments</a></div>
                 </div>
                 <div className="category-list">
-                <div className="category-subheading category-item">Register</div>
+                <div className="category-subheading"><a className="category-item">Register</a></div>
                 </div>
                 <div className="category-list">
-                <div className="category-subheading category-item">Wordle For Rocket League</div>
+                <div className="category-subheading"><a className="category-item">Wordle For Rocket League</a></div>
                 </div>
-
             </nav>
             <nav className="category-content ">
-            <div className="category-heading category-item">f1 22</div>
+            <div className="category-heading"><a className="category-item">f1 22</a></div>
                 <hr className="menu-hr"></hr>
                 <div className="category-list">
-                <div className="category-subheading category-item">Races</div>
-                <div className="category-link category-item">Tier A</div>
-                <div className="category-link category-item">Tier B</div>
+                <div className="category-subheading"><a className="category-item">Races</a></div>
+                <div className="category-link"><a className="category-item">Tier A</a></div>
+                <div className="category-link"><a className="category-item">Tier B</a></div>
                 </div>
                 <div className="category-list">
-                <div className="category-subheading category-item">Drivers</div>
-                <div className="category-link category-item">Tier A</div>
-                <div className="category-link category-item">Tier B</div>
-                <div className="category-subheading category-item">Register</div>
+                <div className="category-subheading"><a className="category-item">Drivers</a></div>
+                <div className="category-link"><a className="category-item">Tier A</a></div>
+                <div className="category-link"><a className="category-item">Tier B</a></div>
+                <div className="category-subheading"><a className="category-item">Register</a></div>
                 </div>
             </nav>
+        </div>
         </div>
     </div>
   )
