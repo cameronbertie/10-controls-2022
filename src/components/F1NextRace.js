ef0a8c58e160be8c4487f200447912afb120f194
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-08-19 21:03:10 +0100

    updated f1 next race

--- src/components/F1NextRace.js
+++ src/components/F1NextRace.js
@@ -14,18 +14,18 @@ function F1NextRace () {
         </div>
         <div className="next-race-box">
           <div className="flag-img-container">
-          <img className="flag-img" src="/au.png"/>
+          <img className="flag-img" src="/be.png"/>
           </div>
-          <h2 className="race-title">Melbourne</h2>
+          <h2 className="race-title">Spa-Francorchamps</h2>
           <div className="next-race-tier-container">
             <div className="next-race-tier-left">
               <h3 className="race-tier">Tier A</h3>
-              <h4 className="race-date">Friday, 5th August 2022</h4>
+              <h4 className="race-date">Friday, 19th August 2022</h4>
               <h4 className="race-time">19:30 BST</h4>
             </div>
             <div className="next-race-tier-right">
               <h3 className="race-tier">Tier B</h3>
-              <h4 className="race-date">Saturday, 6th August 2022</h4>
+              <h4 className="race-date">Saturday, 20th August 2022</h4>
               <h4 className="race-time">19:30 BST</h4>
             </div>
           </div>
